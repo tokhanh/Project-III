59149e29024328c84edd611cafce32398b0fc8ab
tokhanh <kto2303@gmail.com>
2022-01-12 09:30:35 +0700

    move to each folder

--- Implement/client/src/modules/education_management/RegisterUnitManagement.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function RegisterUnitManagement() {
-    return (
-        <div>
-            Hello from Register Unit Management
-        </div>
-    )
-}
