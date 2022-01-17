6ae8ef248bfbd4906bb5a7a74584a97dc1ac0243
tokhanh <kto2303@gmail.com>
2022-01-17 20:39:20 +0700

    fix status of the student

--- Implement/server/models/student.model.js
+++ Implement/server/models/student.model.js
@@ -11,7 +11,7 @@ const studentSchema = new mongoose.Schema({
         required: true,
     },
     status: {
-        type: String,
+        type: Number,
         required: true,
     },
     yearOfAdmission: {
